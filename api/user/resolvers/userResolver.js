db58cd0447b47ac614cd977950d1138bebe340ec
Alisson Amaral <aalissonamaral98@gmail.com>
2021-09-27 11:52:06 -0300

    scalar type e 'novas interações'

--- api/user/resolvers/userResolver.js
+++ api/user/resolvers/userResolver.js
@@ -1,4 +1,18 @@
+const { GraphQLScalarType } = require('graphql')
+
 const userResolvers = {
+    RolesType: {
+        ESTUDANTE: "ESTUDANTE",
+        DOCENTE: "DOCENTE",
+        COORDENACAO: "COORDENACAO"
+    },
+    DateTime: new GraphQLScalarType({
+        name: 'DateTime',
+        description: 'string de data e hora no formato ISO-8601',
+        serialize: (value) => value.toISOString(), //converte dado da base de dados
+        parseValue: (value) => new Date(value), // converte dado através do input através de parametros
+        parseLiteral: (ast) => new Date(ast.value) //converte dado através do input através de argumentos inline
+    }),
     Query: {
     /*
         - root (ou parent): o resultado da chamada no “nível” anterior da query; 
@@ -13,10 +27,13 @@ const userResolvers = {
       user: (root, { id }, { dataSources }) => dataSources.usersAPI.getUserById(id)
     },
     Mutation: {
-      adicionaUser: async (root, user, { dataSources }) => dataSources.usersAPI.adicionaUser(user),
-      atualizaUser: async (root, novosDados, { dataSources }) => dataSources.usersAPI.atualizaUser(novosDados),
-      deletaUser: async (root, { id }, { dataSources }) => dataSources.usersAPI.deletaUser(id)
-    }
+        adicionaUser: async (root, { user }, { dataSources }) => dataSources.usersAPI.adicionaUser(user),
+        atualizaUser: async (root, novosDados, { dataSources }) => {
+          console.log(novosDados)
+          return dataSources.usersAPI.atualizaUser(novosDados)
+        },
+        deletaUser: async (root, { id }, { dataSources }) => dataSources.usersAPI.deletaUser(id)
+      }
   }
   
   module.exports = userResolvers
